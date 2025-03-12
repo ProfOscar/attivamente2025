3b4006cbf57c720a57e4c28968d048dff588b9af
Oscar Cambieri <oscar.cambieri@vallauri.edu>
2025-03-12 10:38:56 +0100

    Inserito dati di test per ruoli e utenti; aggiunto classe PasswordHelper; aggiunto progetto console di test con riferimenti incrociati

---
+++ AttivaMente.Tests/Program.cs
@@ -0,0 +1,13 @@
+﻿using AttivaMente.Core.Models;
+using AttivaMente.Core.Security;
+
+Utente utente = new Utente()
+{
+    Nome = "Mario",
+    Cognome = "Rossi",
+    Email = "mario.rossi@example.com",
+    PasswordHash = PasswordHelper.HashPassword("miapassword123"),
+    RuoloId = 1
+};
+
+Console.WriteLine("Hello, World!");
