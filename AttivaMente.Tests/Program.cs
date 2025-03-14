69cdf6297bb8251d27d1b329ebe7f95bfdffed33
Oscar Cambieri <oscar.cambieri@vallauri.edu>
2025-03-14 09:46:32 +0100

    Aggiunto al progetto test SqlConnection e SqlCommand per l'accesso ai dati sul DB

--- AttivaMente.Tests/Program.cs
+++ AttivaMente.Tests/Program.cs
@@ -1,6 +1,8 @@
 ﻿using AttivaMente.Core.Models;
 using AttivaMente.Core.Security;
+using Microsoft.Data.SqlClient;
 
+#region InMemory
 Ruolo rAdmin = new Ruolo()
 {
     Nome = "Admin"
@@ -16,4 +18,17 @@ Utente utente = new Utente()
     Ruolo = rAdmin
 };
 
-Console.WriteLine($"Hello, {utente}");
+Console.WriteLine($"Utilizzo utente creato in memoria\n{utente}\n\n");
+#endregion
+
+#region SqlServer
+string connStr = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\oscar.cambieri\\Desktop\\attivamente2025\\AttivaMente.Data\\AttivaMenteDB.mdf;Integrated Security=True;Connect Timeout=30";
+using (SqlConnection connection = new SqlConnection(connStr))
+{
+    string sqlQuery = "SELECT * FROM Utenti";
+    using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+    {
+
+    }
+}
+#endregion
