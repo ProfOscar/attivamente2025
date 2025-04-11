db84af0836937c8948d87289c1ef3fdc05ea643f
Oscar Cambieri <oscar.cambieri@vallauri.edu>
2025-04-11 09:46:19 +0200

    Iniziato CRUD Web di Ruoli e Utenti

---
+++ AttivaMente.Web/Controllers/RuoloController.cs
@@ -0,0 +1,22 @@
+﻿using AttivaMente.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AttivaMente.Web.Controllers
+{
+    public class RuoloController : Controller
+    {
+        private readonly RuoloRepository _repo;
+        
+        public RuoloController(IConfiguration configuration)
+        {
+            string connStr = configuration.GetConnectionString("DefaultConnection");
+            _repo = new RuoloRepository(connStr);
+        }
+
+        public IActionResult Index()
+        {
+            var ruoli = _repo.GetAll();
+            return View(ruoli);
+        }
+    }
+}
---
+++ AttivaMente.Web/Controllers/UtenteController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AttivaMente.Web.Controllers
+{
+    public class UtenteController : Controller
+    {
+        public UtenteController(IConfiguration configuration)
+        {
+
+        }
+    }
+}
