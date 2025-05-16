69372ce04f661e77840cfb4ce2635a0bcc959ebf
Oscar Cambieri <oscar.cambieri@vallauri.edu>
2025-05-16 09:43:59 +0200

    Realizzato aggiunta utente (messaggi di errore da perfezionare)

--- AttivaMente.Core/Models/Utente.cs
+++ AttivaMente.Core/Models/Utente.cs
@@ -1,14 +1,24 @@
-﻿namespace AttivaMente.Core.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AttivaMente.Core.Models
 {
     public class Utente
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Il nome è obbligatorio")]
         public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Il cognome è obbligatorio")]
         public string Cognome { get; set; }
+
+        [EmailAddress(ErrorMessage = "Email non valida")]
         public string Email { get; set; }
-        public string PasswordHash { get; set; }
+        
+        public string? PasswordHash { get; set; }
+        
         public int RuoloId { get; set; }
-        public Ruolo Ruolo { get; set; }
+        public Ruolo? Ruolo { get; set; }
 
         public override string ToString()
         {
--- AttivaMente.Web/Controllers/UtenteController.cs
+++ AttivaMente.Web/Controllers/UtenteController.cs
@@ -1,16 +1,21 @@
-﻿using AttivaMente.Data;
+﻿using AttivaMente.Core.Models;
+using AttivaMente.Core.Security;
+using AttivaMente.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace AttivaMente.Web.Controllers
 {
     public class UtenteController : Controller
     {
         private readonly UtenteRepository _repoUtenti;
+        private readonly RuoloRepository _repoRuoli;
 
         public UtenteController(IConfiguration configuration)
         {
             string connStr = configuration.GetConnectionString("DefaultConnection");
             _repoUtenti = new UtenteRepository(connStr);
+            _repoRuoli = new RuoloRepository(connStr);
         }
 
         public IActionResult Index()
@@ -22,8 +27,37 @@ namespace AttivaMente.Web.Controllers
 
         public IActionResult Details(int id)
         {
+            ViewBag.Title = $"Dettaglio Utente {id}";
             var utente = _repoUtenti.GetById(id);
             return utente == null ? NotFound() : View(utente);
+        }
+
+        public IActionResult Create()
+        {
+            ViewBag.Title = "Aggiungi Utente";
+            ViewBag.SelectRuoli = new SelectList(_repoRuoli.GetAll(), "Id", "Nome");
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Utente utente, string password, string confermaPassword)
+        {
+            if (password != confermaPassword)
+            {
+                ModelState.AddModelError("password", "Le password non coincidono.");
+            }
+            else
+            {
+                if (ModelState.IsValid)
+                {
+                    utente.PasswordHash = PasswordHelper.HashPassword(password);
+                    _repoUtenti.Add(utente);
+                    return RedirectToAction("Index");
+                }
+            }
+            ViewBag.Title = "Aggiungi Utente";
+            ViewBag.SelectRuoli = new SelectList(_repoRuoli.GetAll(), "Id", "Nome");
+            return View(utente);
         }
     }
 }
