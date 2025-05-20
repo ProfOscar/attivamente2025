1acea889c33c457a3d09359de328f40e5e4d082b
Oscar Cambieri <oscar.cambieri@vallauri.edu>
2025-05-20 08:41:43 +0200

    Update di Utente funzionante; manca cambio password

--- AttivaMente.Data/UtenteRepository.cs
+++ AttivaMente.Data/UtenteRepository.cs
@@ -84,6 +84,13 @@ namespace AttivaMente.Data
 
         public int Update(Utente utente)
         {
+            // Se password assente, recupero il suo attuale valore
+            if (string.IsNullOrWhiteSpace(utente.PasswordHash))
+            {
+                var esistente = GetById(utente.Id);
+                if (esistente == null) return 0;
+                utente.PasswordHash = esistente.PasswordHash;
+            }
             string sql = "UPDATE Utenti SET " +
                                 "Nome = @nomePlaceholder, Cognome = @cognPlaceholder, " +
                                 "Email = @emailPlaceholder, PasswordHash = @pwHashPlaceholder, RuoloId = @ruoloIdPlaceholder " +
