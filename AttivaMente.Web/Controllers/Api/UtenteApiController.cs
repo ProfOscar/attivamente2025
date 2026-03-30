0a86d2bee82dee53cb490d79d19b75f8c968dc81
Oscar Cambieri <oscar.cambieri@vallauri.edu>
2026-03-30 09:36:07 +0200

    Implementato API GET per filtro con id e ricerca complessa in querystring

--- AttivaMente.Web/Controllers/Api/UtenteApiController.cs
+++ AttivaMente.Web/Controllers/Api/UtenteApiController.cs
@@ -3,8 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AttivaMente.Web.Controllers.Api
 {
-    [ApiController]
-    [Route("api/utenti")]
+    [ApiController] // indispensabile
+    [Route("api/utenti")] // prefisso che vale per tutti gli endpoint del controller
     public class UtenteApiController : ControllerBase
     {
         private readonly UtenteRepository _repoUtenti;
@@ -14,11 +14,32 @@ namespace AttivaMente.Web.Controllers.Api
             _repoUtenti = new UtenteRepository(connStr);
         }
 
-        [Route("all")]
+        [Route("all")] // suffisso route specifica di questo endpoint
         public IActionResult Index()
         {
             var utenti = _repoUtenti.GetAll();
-            return Ok(utenti);
+            return utenti.Count == 0 ? NotFound() : Ok(utenti);
+        }
+
+        //[Route("{id}")]
+        [HttpGet("{id}")] // suffisso di route che esplicita il verbo HttpGet (chiamata in GET)
+        public IActionResult Detail(int id)
+        {
+            var utente = _repoUtenti.GetById(id);
+            return Ok(utente);
+        }
+
+        [HttpGet("find")] // esempio parametrizzazione in querystring
+        public IActionResult Find(
+            string search,
+            int? ruolo,
+            string? order,
+            string? direction)
+        {
+            // tutti i parametri (alcuni facoltativi) sono automaticamente passabili in querystring
+            // https://localhost:7137/api/utenti/find?search=al&ruolo=2&order=Nome&direction=DESC
+            var utenti = _repoUtenti.Search(search, ruolo, order, direction);
+            return utenti.Count == 0 ? NotFound() : Ok(utenti);
         }
     }
 }
