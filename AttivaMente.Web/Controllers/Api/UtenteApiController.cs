7293bbe0836ff4518b5c2ad38408d42b3dee326e
Oscar Cambieri <oscar.cambieri@vallauri.edu>
2026-03-30 09:02:58 +0200

    Implementato prima API per GET lista completa utenti

---
+++ AttivaMente.Web/Controllers/Api/UtenteApiController.cs
@@ -0,0 +1,24 @@
+﻿using AttivaMente.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AttivaMente.Web.Controllers.Api
+{
+    [ApiController]
+    [Route("api/utenti")]
+    public class UtenteApiController : ControllerBase
+    {
+        private readonly UtenteRepository _repoUtenti;
+        public UtenteApiController(IConfiguration configuration)
+        {
+            string connStr = configuration.GetConnectionString("DefaultConnection");
+            _repoUtenti = new UtenteRepository(connStr);
+        }
+
+        [Route("all")]
+        public IActionResult Index()
+        {
+            var utenti = _repoUtenti.GetAll();
+            return Ok(utenti);
+        }
+    }
+}
